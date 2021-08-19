de37251e077c5a45bd743381b876dc784744cc42
William_Twomey <48021997+Reilig35@users.noreply.github.com>
2021-08-19 19:10:19 +0100

    Add project files.

---
+++ Abstract_Classes_Assignment/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract_Classes_Assignment
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            base.SayName();
+        }
+    }
+}
---
+++ Abstract_Classes_Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Abstract_Classes_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Employee Sample = new Employee();
+            Sample.fName = "Sample";
+            Sample.lName = "Student";
+
+            Sample.SayName();
+        }
+    }
+}
